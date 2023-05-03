2b733b75e65f0651456f34eec894e49fa0702350
sjcklink <sjcklink@gmail.com>
2023-05-03 20:47:36 +0200

    done

--- components/ChatBubble.vue
+++ components/ChatBubble.vue
@@ -2,12 +2,14 @@
 // @ts-expect-error I couldn't find any types for vue 3 markdownit
 import Markdown from "vue3-markdown-it";
 import type { Message, User } from "@/types";
+
 defineProps<{
   message?: Message;
   user?: User;
   myMessage?: boolean;
 }>();
 </script>
+
 <template>
   <div
     class="chat"

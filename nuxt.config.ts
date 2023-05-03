f7bad613ec9f8a9d2636350d16858b8889f6576a
sjcklink <sjcklink@gmail.com>
2023-05-03 14:42:23 +0200

    variables changed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
   runtimeConfig: {
-    OPENAI_API_KEY: "",
+    OPENAI_API_KEY: "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",
   },
   ssr: false,
 });
